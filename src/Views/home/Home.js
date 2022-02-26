2e55ffd2c0635dc35ab6655c7e204d2fa69fa70b
rushan-interserv <97483323+rushan-interserv@users.noreply.github.com>
2022-02-27 01:04:41 +1100

    feat: initial table

---
+++ src/Views/home/Home.js
@@ -0,0 +1,55 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+        
+        
+        
+        {/* <table class="table standings-table">
+            <thead >
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Club</th>
+                    <th scope="col">MP</th>
+                    <th scope="col">W</th>
+                    <th scope="col">D</th>
+                    <th scope="col">L</th>
+                    <th scope="col">GF</th>
+                    <th scope="col">GA</th>
+                    <th scope="col">GD</th>
+                    <th scope="col">Pts</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Penshurst United</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>FC Rockdale</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+            </tbody>
+            </table> */}
+    </div>
+  );
+}
+
+export default Home;
